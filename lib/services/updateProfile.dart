ada9923239dcaa4b5d93a016604528d22011b014
NzGhozt7 <s63683@ocean.umt.edu.my>
2024-10-02 10:36:01 +0800

    profil

---
+++ lib/services/updateProfile.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class UpdateProfile {
+  final String _url = 'http://localhost:3000/api/profil/update';
+  final storage = const FlutterSecureStorage();
+
+  Future<Map<String, dynamic>?> updateProfileService({required String name, required String email}) async {
+    final token = await storage.read(key: 'token');
+    final res = await http.put(
+      Uri.parse(_url),
+      headers: <String, String>{
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(<String, String>{
+        'name': name,
+        'email': email,
+      })
+    );
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body) as Map<String, dynamic>;
+    } else {
+      throw Exception('Failed to update profile: ${res.statusCode}');
+    }
+  }
+}
