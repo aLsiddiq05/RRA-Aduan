8841cbf681095c0426a89d42d18caed93cd1856d
NzGhozt7 <s63683@ocean.umt.edu.my>
2024-10-02 12:53:10 +0800

    rtyui

--- lib/widget/adminPage.dart
+++ lib/widget/adminPage.dart
@@ -7,29 +7,25 @@ class AdminPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    mWidth = MediaQuery.of(context).size.width;
+    // mWidth = MediaQuery.of(context).size.width;
+    // mWidth = MediaQuery.of(context).size.height;
     return Padding(
       padding: EdgeInsets.all(10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            width: mWidth,
-            margin: EdgeInsets.all(mWidth * 0.1),
-            child: Card(
-              
-              child: GestureDetector(
-                child: Column(
-                  children: [
-                    Text('Pegawai'),
-                    Icon(Icons.people),
-                  ],
-                ),
-                onTap: () {
-                  print('Button Pegawai');
-                },
+          Card( 
+            child: GestureDetector(
+              child: Column(
+                children: [
+                  Text('Pegawai'),
+                  Icon(Icons.people),
+                ],
               ),
+              onTap: () {
+                print('Button Pegawai');
+              },
             ),
           ),
           SizedBox(height: 10,),
